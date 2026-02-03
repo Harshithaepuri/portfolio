2d4b64d0cd541c4368aac2c5dc2645cf2cbdb8d1
Harshithaepuri <149501186+Harshithaepuri@users.noreply.github.com>
2026-02-03 15:23:51 +0530

    Update projectdata.js

--- src/data/projectdata.js
+++ src/data/projectdata.js
@@ -12,16 +12,16 @@ export const projects = [
         title: "Shopping Cart with Firebase",
         description:
           "A mini e-commerce shopping cart app with login/signup (Firebase Auth), real-time database integration for storing cart data, and responsive UI.",
-        tech: ["React", "Firebase", "Tailwind", "Context API"],
+        tech: ["React", "Firebase", "Tailwind CSS", "Context API"],
         link: "https://react-shoppingcart-firebase.vercel.app/", // live deployed link
       },
 
       {
         id: 3,
-        title: "Song Playlist App",
+        title: "Task Management App",
         description:
-          "Create, edit, and delete playlists. Add songs, play them, and store data persistently using local storage. Simple but fun music app!",
-        tech: ["React", "Tailwind", "LocalStorage"],
-        link: "https://yoursongplaylist.com",
+          "A clean and scalable task manager built with React, Context API, and useReducer. Supports adding, editing, deleting, completing tasks, and filtering by priority and status.",
+        tech: ["React", "Context API", "useReducer", "TypeScript", "Tailwind CSS"],
+        link: "https://tasks-context-reducer.vercel.app/",
       },  
 ]
